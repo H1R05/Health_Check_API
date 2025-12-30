dd6d96599e10de915130d17bbbd86cf441b767ff
Samuele Angelicchio <samueleangelicchio9@gmail.com>
2025-12-30 15:34:47 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.json({
+    messaggio: "Benvenuto",
+  });
+});
+
 app.get("/health", (req, res) => {
   res.status(200).json({
     status: "UP",
