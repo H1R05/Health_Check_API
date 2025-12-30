8b5d20ff8fc9d97a425bd5e7a7753f0355f20f06
Samuele Angelicchio <samueleangelicchio9@gmail.com>
2025-12-30 15:54:47 +0100

    Add file server.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
   res.json({
     messaggio: "Benvenuto",
@@ -21,10 +19,6 @@ app.get("/version", (req, res) => {
   res.json({
     version: "1.0.0",
   });
-});
-
-app.listen(PORT, () => {
-  console.log(`Service running on port ${PORT}`);
 });
 
 module.exports = app;
--- tests/health.test.js
+++ tests/health.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../src/app");
 
 describe("Health Check API", () => {
-  it("Get /health should return UP status", async () => {
+  it("Get /health dovrebbe ritornare status = UP", async () => {
     const res = await request(app).get("/health");
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe("UP");
